b984ed052f497e436620c0f97e6f59704373ad17
Shubham33334 <shubhamtayade965@gmail.com>
2024-08-23 00:41:00 +0530

    Add files via upload

---
+++ Sudoku Sovler.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(int row, int col, vector<vector<char>>& board, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(i, j, board, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) return true;
+                            else board[i][j] = '.';  
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
+
+int main() {
+    // Define the input board
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    // Creating an instance of Solution
+    Solution sol;
+
+    // call the function to solve the sudoku
+    sol.solveSudoku(board);
+
+    // print the solved puzzle
+    for (const auto& row : board) {
+        for (char cell : row) {
+            cout << cell << ' ';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
